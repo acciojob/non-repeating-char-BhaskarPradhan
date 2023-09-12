9f31bd592e1ff1c3000aaf33e36fc48122001543
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-09-12 22:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function firstNonRepeatedChar(str) {
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+let cont=0;
+for(let i=0; i<input.length;i++){
+    for(let j=0;j<input.length;j++){
+    if(input.charAt(i)== input.charAt(j)){
+        cont++;
+    }
+    }
+    if(cont==0){
+        alert(input[i]);
+    }
+    else{
+        alert(null);
+    }
+}
