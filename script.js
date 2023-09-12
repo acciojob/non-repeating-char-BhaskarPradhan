8605b36d45cb2cb66cc0de929cd825b73bcff513
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-09-12 22:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
 let cont=0;
 for(let i=0; i<input.length;i++){
-    for(let j=0;j<input.length;j++){
+    for(let j=1;j<input.length;j++){
     if(input.charAt(i)== input.charAt(j)){
         cont++;
     }
     }
     if(cont==0){
-        alert(input[i]);
+        console.log(input[i]);
     }
     else{
-        alert(null);
+        console.log(null);
     }
 }
