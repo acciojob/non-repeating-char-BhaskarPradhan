0da05fbf9750d716349ed6bfcd1fab151d0e87ed
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-09-12 22:41:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	for(let i=0; i<input.length;i++){
+	for(let i=0; i<str.length;i++){
 	let cont=0;
-    for(let j=1;j<input.length;j++){
-    if(input.charAt(i)== input.charAt(j)){
+    for(let j=1;j<str.length;j++){
+    if(str.charAt(i)== str.charAt(j)){
         cont++;
     }
     }
     if(cont==0){
-        console.log(input.charAt(i));
+        console.log(str.charAt(i));
     }
     else{
         console.log(null);
